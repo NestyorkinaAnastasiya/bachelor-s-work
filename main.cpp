97eaeb271e7dd61e6ba44dd8c84ac8b80e1d9cac
Anastasiya <na_sty191@mail.ru>
2018-05-30 23:05:33 +0700

    .

--- main.cpp
+++ main.cpp
@@ -2,125 +2,162 @@
 
 void FindSolution()
 {
-	// Атрибуты потока
+	// РђС‚СЂРёР±СѓС‚С‹ РїРѕС‚РѕРєР°
 	pthread_attr_t attrs;
-
-
-	// Инициализация атрибутов потока
+	
+	// РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ Р°С‚СЂРёР±СѓС‚РѕРІ РїРѕС‚РѕРєР°
 	if (0 != pthread_attr_init(&attrs))
 	{
 		perror("Cannot initialize attributes");
 		abort();
 	};
 
-	// Установка атрибута "присоединённый"
+	// РЈСЃС‚Р°РЅРѕРІРєР° Р°С‚СЂРёР±СѓС‚Р° "РїСЂРёСЃРѕРµРґРёРЅС‘РЅРЅС‹Р№"
 	if (0 != pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_JOINABLE))
 	{
 		perror("Error in setting attributes");
 		abort();
 	}
 	double sum;
-	// Порождение диспетчера
+	MPI_Request s;
+	MPI_Status st;
+	int exit = -1;
+	// РџРѕСЂРѕР¶РґРµРЅРёРµ РґРёСЃРїРµС‚С‡РµСЂР°
+	if (size != 0)
+		if (0 != pthread_create(&thrs[countOfWorkers], &attrs, dispatcher, &ids[countOfWorkers])) {
+			perror("Cannot create a thread");
+			abort();
+		}
+	// РџРѕСЂРѕР¶РґРµРЅРёРµ РєРѕРЅС‚СЂРѕР»Р»РµСЂР° РєР°СЂС‚
 	if (0 != pthread_create(&thrs[countOfWorkers+1], &attrs, mapController, &ids[countOfWorkers+1]))
 	{
 		perror("Cannot create a thread");
 		abort();
 	}
+	MPI_Comm_dup(currentComm, &serverComm);
+	MPI_Comm_dup(currentComm, &reduceComm);
+	MPI_Comm_dup(currentComm, &barrierComm);
+	// РџРѕСЂРѕР¶РґРµРЅРёРµ СЃРµСЂРІРµСЂР°
+	if (0 != pthread_create(&thrs[countOfWorkers+2], &attrs, server, &ids[countOfWorkers+2]))
+	{
+		perror("Cannot create a thread");
+		abort();
+	}
+	std::vector<int> flags(size);
+	std::vector<int> globalFlags(size);
 
-	for (iteration = 0; iteration < maxiter && residual > eps; iteration++)
+	for (iteration = 0; iteration < maxiter && CheckConditions(); iteration++)
 	{	
-		if (rank == 0) printf("%d::  --------------------START ITERATION %d---------------------\n", rank, iteration);
-		for (auto i : newResult) i = 0;
-		for (auto i : oldResult) i = 0;
 		
-		oldMap = newMap;
+		if (rank == 0) printf("%d::  --------------------START ITERATION %d---------------------\n", rank, iteration);
+		for (auto &i : newResult) i = 0;
+		for (auto &i : oldResult) i = 0;
+		
 		while (!allTasks.empty()) {
-			Task *t = allTasks.front();					
+			
+			Task *t = allTasks.front();
+			if(iteration != 0) t->ReceiveFromNeighbors(currentComm);				
+			pthread_mutex_lock(&mutex_get_task);
 			currentTasks.push(t);
+			pthread_mutex_unlock(&mutex_get_task);
 			allTasks.pop();
+			
 		}
 		
-		/*printf("%d:: block = %d, lock = %d, tpp = %d, f = %d, fs = %d\n",rank,currentTasks.back()->blockNumber,currentTasks.back()->localNumber, currentTasks.back()->tasks_x, currentTasks.back()->flag, currentTasks.back()->firstStart );
-		printf("existRecv: %d %d %d %d %d %d\n", currentTasks.back()->existRecv[0], currentTasks.back()->existRecv[1], currentTasks.back()->existRecv[2], currentTasks.back()->existRecv[3], currentTasks.back()->existRecv[4], currentTasks.back()->existRecv[5]);
-		printf("neighbors: %d %d %d %d %d %d\n", currentTasks.back()->neighbors[0], currentTasks.back()->neighbors[1], currentTasks.back()->neighbors[2], currentTasks.back()->neighbors[3], currentTasks.back()->neighbors[4], currentTasks.back()->neighbors[5]);
-				
-		printf("oldU:\n");
-		for (int z = 0; z < currentTasks.back()->oldU.size();z++)
-			printf("oldU[%d] = %lf\n", z, currentTasks.back()->oldU[z]);
-		printf("\n");
-		printf("newU:\n");
-		for (int z = 0; z <  currentTasks.back()->newU.size();z++)
-			printf("newU[%d] = %lf\n", z,  currentTasks.back()->newU[z]);
-		printf("\n");*/
-		printf("%d:: SIZE = %d\n",rank,currentTasks.size());
+		fprintf(stderr, "%d:: count of tasks = %d\n", rank, currentTasks.size());
 
-		// Порождение рабочих потоков
+		// РџРѕСЂРѕР¶РґРµРЅРёРµ СЂР°Р±РѕС‡РёС… РїРѕС‚РѕРєРѕРІ
 		for (int i = 0; i < countOfWorkers; i++)
 			if (0 != pthread_create(&thrs[i], &attrs, worker, &ids[i])) {
 				perror("Cannot create a thread");
 				abort();
 			}
-		
-		// Порождение диспетчера
-		if (size != 1)
-		if (0 != pthread_create(&thrs[countOfWorkers], &attrs, dispatcher, &ids[countOfWorkers])) {
-			perror("Cannot create a thread");
-			abort();
-		}
-
-		// Ожидание завершения порожденных потоков
-		if (size != 1) {
-			for (int i = 0; i < countOfThreads-1; i++)
-				if (0 != pthread_join(thrs[i], NULL)) {
-					perror("Cannot join a thread");
-					abort();
-				}
-		}
-		else {	
-			for (int i = 0; i < countOfWorkers; i++)
-				if (0 != pthread_join(thrs[i], NULL)) {
-					perror("Cannot join a thread");
-					abort();
-				}
-		}
-
-		sum = 0;
-		// Расчёт абсолютной погрешности между текущим и предыдущим решениями
-		for (int i = 0; i < newResult.size(); i++)
-			sum += (newResult[i] - oldResult[i])*(newResult[i] - oldResult[i]);
-
-		MPI_Allreduce(&sum, &residual, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-		residual = sqrt(residual);
-		printf("%d:: res = %lf\n",rank, residual );
-		MPI_Allreduce(newResult.data(), globalRes.data(), newResult.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-		/*if (rank == 0) {
 			
-			// Вывод результата
-			//if (iteration == 55){
-				printf("\n--------------------------------------------------------------------\n\n");
-				double sum = 0;
-				for (int i = 0; i < globalRes.size(); i++)
-				{
-					sum += (2. - globalRes[i])*(2. - globalRes[i]);
-					printf("%.14lf\n", newResult[i]);
-				}	
-				sum = sqrt(sum);
-				printf("||result|| = %.10e\n", sum);
-				printf("\n--------------------------------------------------------------------\n");
-			//}
-		}*/
-		if (rank == 0) printf("%d:: --------------------FINISH ITERATION %d---------------------\n", rank, iteration);
+		// РћР¶РёРґР°РЅРёРµ Р·Р°РІРµСЂС€РµРЅРёСЏ РїРѕСЂРѕР¶РґРµРЅРЅС‹С… РїРѕС‚РѕРєРѕРІ
+		for (int i = 0; i < countOfWorkers; i++)
+			if (0 != pthread_join(thrs[i], NULL)) {
+				perror("Cannot join a thread");
+				abort();
+			}
+
+		fprintf(stderr,"%d::workers closed\n", rank);	
+		bool change = false;
+		flags[rank] = changeComm;
+		
+		MPI_Allreduce(flags.data(), globalFlags.data(), globalFlags.size(), MPI_INT, MPI_SUM, reduceComm);
+		for (int i = 0; i < globalFlags.size() && !change; i++)
+			if (globalFlags[i]) change = true;
+		// Р•СЃР»Рё РїСЂРѕРёСЃС…РѕРґРёР»Рѕ РёР·РјРµРЅРµРЅРёРµ РєРѕРјРјСѓРЅРёРєР°С‚РѕСЂР° РІ С…РѕРґРµ СЂР°Р±РѕС‚С‹ РїРѕС‚РѕРєРѕРІ
+		// С‚Рѕ РїРѕСЃС‹Р»Р°РµРј СЃРѕРѕР±С‰РµРЅРёРµ, С…Р°СЂР°РєС‚РµСЂРёР·СѓСЋС‰РµРµ Р·Р°РєСЂС‹С‚РёРµ СЃС‚Р°СЂРѕРіРѕ РєРѕРјРјСѓРЅРёРєР°С‚РѕСЂР°
+		if (change) {
+			
+			int cond = 4; 
+			// РћС‚РїСЂР°РІРєР° СЃРѕРѕР±С‰РµРЅРёСЏ Рѕ РЅРµРѕР±С…РѕРґРёРјРѕСЃС‚Рё Р·Р°РєСЂС‹С‚СЊ СЃС‚Р°СЂС‹Р№ РєРѕРјРјСѓРЅРёРєР°С‚РѕСЂ			
+			MPI_Send(&cond, 1, MPI_INT, rank, 2001, currentComm);
+			fprintf(stderr,"%d::send to old_dispetcher sucsess\n", rank);
+			
+			if (rank == 0) 
+				for(int k = size_old; k < size; k++) 
+				MPI_Send(&iteration, 1, MPI_INT, k, 10005, newComm);
+			
+			fprintf(stderr,"%d::start dup\n", rank);
+			MPI_Comm_dup(newComm, &serverComm);
+			fprintf(stderr,"%d::dup server sucsess\n", rank);
+			MPI_Comm_dup(newComm, &reduceComm);
+			fprintf(stderr,"%d::dup reduce sucsess\n", rank);
+			MPI_Comm_dup(newComm, &barrierComm);
+			while (!changeComm); // СѓСЃР»РѕРІРЅР°СЏ РїРµСЂРµРјРµРЅРЅР°СЏ
+			currentComm = newComm;
+			flags.resize(size);
+			globalFlags.resize(size);
+			changeComm = false;
+			server_new = false;
+		}
+			
+		GenerateResultOfIteration(reduceComm);
+
+		while (!queueRecv.empty()) {
+			Task *t = queueRecv.front();
+			t->SendToNeighbors(currentComm);
+			queueRecv.pop();		
+			allTasks.push(t);
+			
+		}
+
+		if (rank == 0) {
+			printf("%d:: res = %e\n",rank, residual );
+			printf("%d:: --------------------FINISH ITERATION %d---------------------\n", rank, iteration);
+			
+		}
 	}
-	MPI_Request s;
-	int exit = -1;
-	MPI_Isend(&exit, 1, MPI_INT, rank, 1030, MPI_COMM_WORLD, &s);
-	if (0 != pthread_join(thrs[countOfThreads-1], NULL)) {
-		perror("Cannot join a thread");
-		abort();
+	MPI_Isend(&exit, 1, MPI_INT, rank, 1030, currentComm, &s);
+	MPI_Isend(&exit, 1, MPI_INT, rank, 2001, currentComm, &s);
+	pthread_join(thrs[countOfWorkers], NULL);
+	fprintf(stderr,"%d::dispetcher close\n", rank);
+	// Р•СЃР»Рё РїСЂРѕС†РµСЃСЃС‹ РїРѕРґСЃРѕРµРґРёРЅРёР»РёСЃСЊ РїРѕcР»Рµ РІСЃРµС… РІС‹С‡РёСЃР»РµРЅРёР№
+	if (rank == 0){
+		int cond, flag, size_new;
+		while (numberOfConnection < countOfConnect) {
+			MPI_Recv(&cond, 1, MPI_INT, rank, 2001, currentComm, &st);
+			size_old = size;
+			// Р’С‹С‡РёСЃР»СЏРµРј РЅРѕРІС‹Р№ СЂР°Р·РјРµСЂ Рё rР°nk
+       			MPI_Comm_size(newComm, &size_new);
+	
+			cond = 0;
+			for(int k = size_old; k < size_new; k++) 
+				MPI_Send(&cond, 1, MPI_INT, k, 10000, newComm);			
+				
+			server_new = false;
+
+		}
 	}
-	// Освобождение ресурсов, занимаемых описателем атрибутов
-	pthread_attr_destroy(&attrs);
+	pthread_join(thrs[countOfWorkers+1], NULL);	
+	fprintf(stderr,"%d::mapController close\n", rank);
+	pthread_join(thrs[countOfWorkers+2], NULL);
+	fprintf(stderr,"%d::server close\n", rank);
+	
+	// РћСЃРІРѕР±РѕР¶РґРµРЅРёРµ СЂРµСЃСѓСЂСЃРѕРІ, Р·Р°РЅРёРјР°РµРјС‹С… РѕРїРёСЃР°С‚РµР»РµРј Р°С‚СЂРёР±СѓС‚РѕРІ
+	//pthread_attr_destroy(&attrs);
 }
 
 int main(int argc, char **argv)
@@ -131,47 +168,32 @@ int main(int argc, char **argv)
 		std::cerr << "not MPI_THREAD_MULTIPLE";
 		exit(0);
 	}
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	//Получаем количество узлов
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Status st;
-	// Инициализация мьютекса
-	pthread_mutexattr_t attr;
-	pthread_mutexattr_init(&attr);
-	pthread_mutex_init(&mutex, &attr);
+
+	MPI_Comm_rank(currentComm, &rank);
+	//РџРѕР»СѓС‡Р°РµРј РєРѕР»РёС‡РµСЃС‚РІРѕ СѓР·Р»РѕРІ
+	MPI_Comm_size(currentComm, &size);
+
+	// РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РјСЊСЋС‚РµРєСЃР°
+	pthread_mutexattr_t attr_get_task;
+	pthread_mutexattr_init(&attr_get_task);
+	pthread_mutex_init(&mutex_get_task, &attr_get_task);
+	
+	pthread_mutexattr_t attr_set_task;
+	pthread_mutexattr_init(&attr_set_task);
+	pthread_mutex_init(&mutex_set_task, &attr_set_task);
+
+	GenerateBasicConcepts();	
 
 	GenerateQueueOfTask();
+	
+	std::vector<int> tmp(map.size());
+	MPI_Allreduce(map.data(), tmp.data(), map.size(), MPI_INT, MPI_SUM, currentComm);
+	map = tmp;
+
 	FindSolution();
 
-
-	MPI_Allreduce(newResult.data(), globalRes.data(), newResult.size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-	//MPI_Reduce(timer.data(), time.data(), time.size(), MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	//MPI_Reduce(iterations.data(), resIterations.data(), resIterations.size(), MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	//MPI_Reduce(res.data(), globalR.data(), res.size(), MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	GenerateResult(currentComm);
 	
-	/*unsigned int allTime = 0;
-	for (int i = 0; i < size; i++)
-		if (allTime < time[i])
-			allTime = time[i];*/
-
-	// Вывод результата
-	if (rank == 0)
-	{
-		printf("\n--------------------------------------------------------------------\n\n");
-		double sum = 0;
-		for (int i = 0; i < globalRes.size(); i++)
-		{
-			sum += (2. - globalRes[i])*(2. - globalRes[i]);
-			printf("%.14lf\n", globalRes[i]);
-		}
-		sum = sqrt(sum);
-		printf("||result|| = %.10e\n", sum);
-		//printf("dimention: %d\tcountOfProcess: %d\tallTime: %d\n\n",dim, size, allTime);
-		/*for (int i = 0; i < size; i++)
-			printf("rank %d::\ttime = %d;\tcountOfIter = %d;\tresidual = %e\n", i, time[i], resIterations[i], globalR[i]);*/
-		printf("\n--------------------------------------------------------------------\n");
-	}
-
 	MPI_Finalize();
 	return 0;
 }
