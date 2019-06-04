1f00cd10fef2ce09a3b8a9737c40e0ef3bbef946
Анастасия Нестёркина < >
2019-06-04 15:03:51 +0700

    a small errors are eliminated

--- library.h
+++ library.h
@@ -17,6 +17,7 @@
 #include <stddef.h>
 #include <sstream>
 #define MAX_DATA 1000
+
 class ITask {
 public:
 	int blockNumber;
@@ -26,18 +27,28 @@ public:
 	void virtual GenerateSend(int reciever, MPI_Comm Comm) = 0;
 };
 
+// Descriptors for threads
 pthread_t thrs[11];
 // id for threads
 int ids[11] = { 0,1,2,3,4,5,6,7,8,9,10 };
-// descriptors for threads
-int changeComm = false;
-bool server_new = false;
+int numberOfConnection = 0;
+int rank, size, rank_old, size_old;
+int countOfConnect = 2;
 // Count of computational threads
 int countOfWorkers = 1;
 // Count of all threads
 int countOfThreads = 3;
+int changeComm = false;
+bool changeExist = false;
+bool server_new = false;
+std::vector<int> map;
+std::queue<ITask*> currentTasks, queueRecv, sendedTasks;
+// Communicators
+MPI_Comm currentComm = oldComm = newComm = 
+	serverComm = reduceComm	= MPI_COMM_WORLD;
 
 pthread_mutexattr_t attr_set_task, attr_get_task;
+pthread_mutex_t mutex_get_task, mutex_set_task;
 pthread_attr_t attrs_dispatcher, attrs_server, attrs_mapController, attrs_workers;
 pthread_cond_t server_cond, comunicator_cond;
 pthread_mutex_t server_mutexcond, comunicator_mutexcond;
@@ -61,18 +72,6 @@ void SendTask(MPI_Status &st, MPI_Comm &CommWorker, MPI_Comm &CommMap);
 void ExecuteOwnTasks();
 void ExecuteOtherTask(MPI_Comm &Comm, int id, bool &retry);
 void ChangeCommunicator(MPI_Comm &Comm, int &newSize);
-
-int numberOfConnection = 0;
-pthread_mutex_t mutex_get_task, mutex_set_task;
-int rank, size;
-int rank_old, size_old;
-std::vector<int> map;
-int countOfConnect = 2;
-bool changeExist = false;
-std::queue<ITask*> currentTasks, queueRecv, sendedTasks;
-// Communicators
-static MPI_Comm currentComm = oldComm = newComm = 
-	serverComm = reduceComm	= MPI_COMM_WORLD;
 void LibraryInitialize(int argc, char **argv, bool clientProgram);
 void CreateLibraryComponents();
 void StartWork();
